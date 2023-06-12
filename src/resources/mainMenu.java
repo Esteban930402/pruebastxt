5f9543239eb062f84541651dda104c168370b9e9
Esteban93040 <espinosa.esteban@correounivalle.edu.co>
2023-06-12 10:35:09 -0500

    asdy

--- src/resources/mainMenu.java
+++ src/resources/mainMenu.java
@@ -6,28 +6,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class mainMenu extends JFrame  {
     private Timer timer;
     private Timer wordTimer;
-    private String name;
-    private boolean trueOrFalse;
+    private String name, database,file;
+    private int level;
     private JButton initGame,rules,yesButton,noButton;
 
     private JPanel principalPanel,buttonPanel,textPanel,counterPanel;
     private JTextField playerUsername;
-
     private Escucha escucha;
     private int comparer=0;
     private JLabel textTimer;
     private List<String> wordsToMemorize;
     private List<String> theOtherWords;
     private List<String> selectdWords;
-    private List<String> noWords;
-
 
     public mainMenu(){
         initGUI();
@@ -43,8 +39,10 @@ public class mainMenu extends JFrame  {
     }
 
     private void initGUI() {
-        name = new String();
 
+        file= new String();
+        database = new String();
+        name = new String();
         timer = new Timer(500, new ActionListener() {
             private int counter = 0;
 
@@ -61,9 +59,7 @@ public class mainMenu extends JFrame  {
             }
         });
 
-
         textTimer = new JLabel("HOLA");
-
         yesButton=new JButton("Yes");
         yesButton.setBackground(Color.green);
         yesButton.setOpaque(true);
@@ -100,7 +96,6 @@ public class mainMenu extends JFrame  {
 
         counterPanel.add(yesButton);
         counterPanel.add(noButton);
-
 
         imagePanel backgroundImage = new imagePanel("/resources/GuiFiles/mainMenuBackground.jpg");
         backgroundImage.setLayout(new BorderLayout());
@@ -141,12 +136,9 @@ public class mainMenu extends JFrame  {
         System.out.println(palabrasMezcladas);
         Collections.shuffle(palabrasMezcladas);
         System.out.println(palabrasMezcladas);
-
-
         JOptionPane.showMessageDialog(null, "Select Yes if the word was in the ones shown above, if it was not found press No, you have 5 seconds to do it");
         yesButton.setEnabled(false);
         noButton.setEnabled(false);
-
         wordTimer = new Timer(2000, new ActionListener() {
             private int counter =0;
             @Override
@@ -168,42 +160,74 @@ public class mainMenu extends JFrame  {
                     int palabrasAmemorizar= wordsToMemorize.size();
                     int porcentaje = (comparer*100)/palabrasAmemorizar;
                     System.out.println("Porcentaje de coincidencia: " + porcentaje + "%");
-
-
+                    int porcentajeParaAvanzar=0;
+                    switch (level){
+                        case 1:
+                            porcentajeParaAvanzar=70;
+                            break;
+                        case 2:
+                            porcentajeParaAvanzar=70;
+                            break;
+                        case 3:
+                            porcentajeParaAvanzar=75;
+                            break;
+                        case 4:
+                            porcentajeParaAvanzar=80;
+                            break;
+                        case 5:
+                            porcentajeParaAvanzar=80;
+                            break;
+                        case 6:
+                            porcentajeParaAvanzar=85;
+                            break;
+                        case 7:
+                            porcentajeParaAvanzar=90;
+                            break;
+                        case 8:
+                            porcentajeParaAvanzar=90;
+                            break;
+                        case 9:
+                            porcentajeParaAvanzar=95;
+                            break;
+                        case 10:
+                            porcentajeParaAvanzar=100;
+                            break;
+                    }
+                    System.out.println(porcentajeParaAvanzar);
+                    if (porcentaje>=porcentajeParaAvanzar){
+                        filesManager filesManager = new filesManager();
+                        int currentLevel=filesManager.manageName(database,name);
+                        int newLevel= currentLevel+1;
+                        filesManager.updateLevel(database,name,newLevel);
+                        JOptionPane.showMessageDialog(null,"Congratulations, press ok if you are ready for the next level");
+                        initGame();
+                    }
                 }
             }
         });
         wordTimer.start();
-
     }
 
     public void initGame(){
 
         selectdWords = new ArrayList<>();
-
         principalPanel.removeAll();
         principalPanel.revalidate();
         principalPanel.repaint();
-
-
         principalPanel.add(textPanel,BorderLayout.CENTER);
         principalPanel.add(counterPanel,BorderLayout.SOUTH);
         yesButton.setEnabled(false);
         noButton.setEnabled(false);
-
         principalPanel.repaint();
         principalPanel.revalidate();
-
         filesManager filesManager = new filesManager();
-        String database = "C:\\Users\\Owner\\IdeaProjects\\pruebastxt\\src\\resources\\database.txt";
-        String file = "C:\\Users\\Owner\\IdeaProjects\\pruebastxt\\src\\resources\\words.txt";
+        database = "C:\\Users\\Owner\\IdeaProjects\\pruebastxt\\src\\resources\\database.txt";
+        file = "C:\\Users\\Owner\\IdeaProjects\\pruebastxt\\src\\resources\\words.txt";
         name = playerUsername.getText();
         filesManager.manageName(database,name);
-        int level = filesManager.manageName(database,name);
-
+        level = filesManager.manageName(database,name);
         try {
             List<String> randomLines = filesManager.getRandomLines(file, level);
-
             for (String linea : randomLines) {
                 // System.out.println(linea);
             }
@@ -212,25 +236,19 @@ public class mainMenu extends JFrame  {
             System.out.println(ammountToMemorize);
             wordsToMemorize=randomLines.subList(0,ammountToMemorize);
             theOtherWords=randomLines.subList(ammountToMemorize,totalLines);
-
             System.out.println(wordsToMemorize+"Palabra a memorizar\n");
             System.out.println(theOtherWords+"NO ");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
        timer.start();
-
     }
-
     public static void main(String[] args){
         EventQueue.invokeLater(() -> {
             mainMenu miProjectGUI = new mainMenu();
         });
     }
-
     private class Escucha implements ActionListener {
-        private int counter = 0; // Agrega una variable de contador
-
         @Override
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == initGame) {
